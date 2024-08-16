d239643c54bcb8806630a287d407fe37e9f065ed
Grace Nieboer <grace.nieboer@outlook.com>
2024-08-16 18:49:55 -0400

    create Nav component

--- src/App.js
+++ src/App.js
@@ -13,9 +13,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <Switch>
           <Route path="/">
             <Home />
---
+++ src/components/Nav.js
@@ -0,0 +1,7 @@
+export const Nav = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
