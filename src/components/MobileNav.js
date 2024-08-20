2a3992896b60986532a079d3d2b6f427465c6166
Grace Nieboer <grace.nieboer@outlook.com>
2024-08-20 18:17:29 -0400

    add links and a lil mobile menu pop up ising state

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 
 export default function MobileNav () {
+    const [open, setOpen] = useState(false)
+    const handleMenu = () => {
+        setOpen(!open)
+    }
+
     return(
-        <div className="bg-lightO flex md:hidden">
-            <div>MOBILE NAV!!!</div>
+        <div className="bg-darkO flex md:hidden">
+            <div className="" 
+            onClick={handleMenu}>
+                MENU ICON
+            </div>
+
+        { open? 
+        <div className="w-screen bg-darkO flex flex-col items-centerm justify-center">
+            <div><a className="p-2" href="/about" alt="">US</a></div>
+            <div><a className="p-2" href="/timeline" alt="">THE TIMELINE</a></div>
+            <div><a className="p-2" href="/bridalparty">THE BRIDAL PARTY</a></div>
+        </div> 
+        : <></>}
 
         </div>
     )
