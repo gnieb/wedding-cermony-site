ba18d8d45f8d1f09cdf11375206dd40ad845ffa1
Grace Nieboer <grace.nieboer@outlook.com>
2024-08-23 23:11:33 -0400

    create timeline page

--- src/pages/BridalParty.js
+++ src/pages/BridalParty.js
@@ -1,30 +1,32 @@
 export default function BridalParty () {
     return(
         <div className="sm:flex font-serif sm:m-0 mx-auto">
-            <div className="p-5 text-5xl text-center">THE BRIDAL PARTY</div>
-            <div className="p-5 text-3xl text-center">
-                <div>Morgan O'Shea</div>
-                <div>Abby Knowlton</div>
-                <div>Sarah Alvarado</div>
-                <div>Paige Bixby</div>
-                <div>McKenzie Fiege</div>
-                <div>Heather Boss</div>
-                <div>Emma Eich</div>
-                <div>Isabelle Consul</div>
-                <div>Jacquelyn Diaz</div>
-            </div>
-            <div className="p-5 py-10 text-3xl text-center">
-                <div>Sam McClarin</div>
-                <div>Noah Bixby</div>
-                <div>Gage Forster</div>
-                <div>Adam Bixby</div>
-                <div>Kyle Navratil</div>
-                <div>John Mitchell</div>
-                <div>Casey Eich</div>
-                <div>Luca Prisco</div>
-                <div>Clark Consul</div>
-                <div>Nathan Liberti</div>
+            <div className="p-5 text-5xl text-center items-center flex">THE BRIDAL PARTY</div>
+            <div>
+                <div className="p-5 text-3xl text-center sm:text-right">
+                    <div>Morgan O'Shea</div>
+                    <div>Abby Knowlton</div>
+                    <div>Sarah Alvarado</div>
+                    <div>Paige Bixby</div>
+                    <div>McKenzie Fiege</div>
+                    <div>Heather Boss</div>
+                    <div>Emma Eich</div>
+                    <div>Isabelle Consul</div>
+                    <div>Jacquelyn Diaz</div>
+                </div>
+                <div className="p-5 py-10 text-3xl text-center">
+                    <div>Sam McClarin</div>
+                    <div>Noah Bixby</div>
+                    <div>Gage Forster</div>
+                    <div>Adam Bixby</div>
+                    <div>Kyle Navratil</div>
+                    <div>John Mitchell</div>
+                    <div>Casey Eich</div>
+                    <div>Luca Prisco</div>
+                    <div>Clark Consul</div>
+                    <div>Nathan Liberti</div>
                     
+                </div>
             </div>
         </div>
     );
---
+++ src/pages/Timeline.js
@@ -0,0 +1,7 @@
+export default function Timeline () {
+    return(
+        <div>
+            <div>10.05.2024</div>
+        </div>
+    )
+}
