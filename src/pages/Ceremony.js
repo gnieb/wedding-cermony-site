b5400e7cedb7df9793cfe5d4916ffd6db033e2f1
Grace Nieboer <grace.nieboer@outlook.com>
2024-08-24 22:50:45 -0400

    just the ceremony page

---
+++ src/pages/Ceremony.js
@@ -0,0 +1,20 @@
+export default function Ceremony () {
+    return(
+        <div>
+            <div className="pt-10 w-full font-bold text-4xl flex justify-center items-center">THE CEREMONY</div>
+            <div className=" font-serif w-full text-2xl flex justify-center items-center">Chapel Hill</div>
+            <div className=" font-serif w-full text-2xl flex justify-center items-center">1:30 PM</div>
+            {/* Event Timeline */}
+            <div>
+                <div className="flex pt-10 font-bold text-2xl w-full justify-center">Please enjoy the ceremony with us - we really want to see your faces, not your devices! As we tie the knot, please be our guest. Our photographer will take care of the rest!</div>
+                <div className="flex justify-center items-center text-center ">
+                    <div className="text-xl">
+                        <p>Chapel Hill</p>
+                        <p>1:30 PM</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,7 +1,19 @@
 export default function Timeline () {
     return(
         <div>
-            <div>10.05.2024</div>
+            <div className="pt-10 w-full font-bold text-4xl flex justify-center items-center">The McClarins</div>
+            <div className=" font-serif w-full text-2xl flex justify-center items-center">10.05.2024</div>
+            {/* Event Timeline */}
+            <div>
+                <div className="flex pt-10 font-bold text-2xl w-full justify-center">THE CEREMONY</div>
+                <div className="flex justify-center items-center text-center ">
+                    <div className="text-xl">
+                        <p>Chapel Hill</p>
+                        <p>1:30 PM</p>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
