9c46ddba9c5193651f3115cd7fc1f59cc4422316
Grace Nieboer <grace.nieboer@outlook.com>
2024-08-28 22:14:41 -0400

    styling, add the icon

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons"
+
 export default function Timeline () {
     return(
         
@@ -9,7 +12,11 @@ export default function Timeline () {
                 <div className="flex pt-10 font-bold text-2xl justify-start ">THE CEREMONY</div>
                 <div className="flex justify-start items-center ">
                     <div className="sm:text-xl">
-                        <p>Chapel Hill</p>
+                        <div className="flex">
+                            <p className=""><FontAwesomeIcon icon={faLocationDot} /></p>
+                            <p className="px-2">Chapel Hill</p>
+                        </div>
+                        
                         <p>1:30 PM</p>
                     </div>
                 </div>
@@ -19,7 +26,11 @@ export default function Timeline () {
                 <div className="flex pt-10 font-bold text-2xl w-full justify-start">SOCIAL HOUR</div>
                 <div className="flex justify-start items-center">
                     <div className="sm:text-xl">
-                        <p>The Greenhouse at the Port of Rochester</p>
+                        <div className="flex">
+                            <p className=""><FontAwesomeIcon icon={faLocationDot} /></p>
+                            <p className="px-2">The Greenhouse at the Port of Rochester</p>
+                        </div>
+                        <a className="underline font-serif italic" rel="noreferrer" href="https://www.google.com/maps/dir//1000+N+River+St,+Rochester,+NY+14612/@43.2556887,-77.6901864,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89d6b79df3c5e215:0x3190c8b68dffa0e7!2m2!1d-77.6077857!2d43.2557183?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D" target="_blank">DIRECTIONS</a>
                         <p>3:30 PM</p>
                     </div>
                 </div>
@@ -29,9 +40,11 @@ export default function Timeline () {
                 <div className="flex font-bold text-2xl w-full justify-start">RECEPTION</div>
                 <div className="flex justify-start items-center ">
                     <div className="sm:text-xl">
-                        <p>The Greenhouse at the Port of Rochester</p>
+                        <div className="flex">
+                            <p className=""><FontAwesomeIcon icon={faLocationDot} /></p>
+                            <p className="px-2">The Greenhouse at the Port of Rochester</p>    
+                        </div>
                         <p>5:30 PM</p>
-                        <a className="underline" rel="noreferrer" href="https://www.google.com/maps/dir//1000+N+River+St,+Rochester,+NY+14612/@43.2556887,-77.6901864,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89d6b79df3c5e215:0x3190c8b68dffa0e7!2m2!1d-77.6077857!2d43.2557183?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D" target="_blank">DIRECTIONS</a>
                     </div>
                 </div>
             </div>
