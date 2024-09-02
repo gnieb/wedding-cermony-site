9b4fa194f7c88172efa24ddf2cb5ba8d9c77a2ab
Grace Nieboer <grace.nieboer@outlook.com>
2024-09-01 23:38:29 -0400

    update the desktop nav

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,6 +1,6 @@
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,10 +5,10 @@ export const Nav = () => {
     return (
         <div className="hidden font-bold h-12 md:flex ">
             <div >
-                <a className="m-5"  alt="" href="/">10.05.2024</a>
+                <a className="m-5"  alt="" href="#/timeline">THE TIMELINE</a>
             </div>
             <div>
-                <a className="m-5" alt="" href="/bridalparty">THE BRIDAL PARTY</a>
+                <a className="m-5" alt="" href="/#bridalparty">THE BRIDAL PARTY</a>
             </div>
         </div>
     );
