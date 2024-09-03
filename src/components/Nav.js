04561a442958099a85f2803319d90ac97c3d5f89
Grace Nieboer <grace.nieboer@outlook.com>
2024-09-02 20:51:31 -0400

    add the zola site

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,6 +10,15 @@ export const Nav = () => {
             <div>
                 <a className="m-1 fixed top-14 bg-darkO rounded-full p-2" alt="" href="/#bridalparty">THE BRIDAL PARTY</a>
             </div>
+            <div>
+               <a
+                className="m-1 fixed bg-darkO top-[105px] rounded-full p-2"
+                href="https://www.zola.com/wedding/graceandnoahgetmarried"
+                target="_blank"
+                rel="noreferrer" >ZOLA SITE</a>
+            </div>
+
+
         </div>
     );
 };
